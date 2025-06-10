676f899fe6439263d4c6a31dcfa6394f8ba72657
Entony Maxwell <entonymaxwell01@gmail.com>
2025-06-09 21:01:33 -0300

    Mapeando elementos da pagina de cadastro

---
+++ cypress/elements/CadastroPage.elements.js
@@ -0,0 +1,10 @@
+export const CadastroPageElements = {
+  cadastroButton:
+    "button.style__ContainerButton-sc-1wsixal-0.ihdmxA.button__child",
+  emailInput: ".card__register input[name='email']",
+  nomeInput: ".card__register input[name='name']",
+  senhaInput: ".card__register input[name='password']",
+  confirmarSenhaInput: ".card__register input[name='passwordConfirmation']",
+  saldoLabel: "label#toggleAddBalance",
+  cadastrarButtonConfirm: ".card__register button[type='submit']",
+};
