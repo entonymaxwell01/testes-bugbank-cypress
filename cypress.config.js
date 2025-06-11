e974522c953603c7707e4e42f2227f1a30fafe04
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-06-11 15:14:37 -0300

    Configurando geração de reports por data

--- cypress.config.js
+++ cypress.config.js
@@ -3,24 +3,22 @@ const dayjs = require("dayjs");
 const reportFolder = `cypress/reports/${dayjs().format("DD-MM-YYYY_HH-mm-ss")}`;
 
 module.exports = defineConfig({
-  reporter: "cypress-mochawesome-reporter",
-  reporterOptions: {
-    reportDir: reportFolder,
-    charts: true,
-    reportPageTitle: "Relatório de Testes",
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-    html: true,
-    json: true,
-    overwrite: false,
-    timestamp: "dd-mm-yyyy_HH-MM-ss",
-    reportDir: "cypress/reports",
-  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
     baseUrl: "https://bugbank.netlify.app",
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: reportFolder,
+      charts: true,
+      reportPageTitle: "Relatório de Testes",
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+      html: true,
+      json: true,
+      overwrite: false,
+    },
   },
 });
