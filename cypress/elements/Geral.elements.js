30e8fcae67b9b38602d3c97a8d28e8ce390eb667
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-06-11 15:15:06 -0300

    Alterando nome do arquivo de geral para notificação elements

--- cypress/elements/Geral.elements.js
+++
@@ -1,4 +0,0 @@
-export const NotificacaoElements = {
-  textoRetorno: "p#modalText",
-  botaoFechar: "a#btnCloseModal",
-};
---
+++ cypress/elements/Notificacao.elements.js
@@ -0,0 +1,6 @@
+export const NotificacaoElements = {
+  textoRetorno: "p#modalText",
+  botaoFechar: "a#btnCloseModal",
+  campoObrigatorioMensagem: "p.input__warging",
+  containerInformacao: "div.styles__ContainerInformations-sc-8zteav-3.fQkeSa",
+};
