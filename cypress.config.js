29a0275bf327994fe37e4382599f8e6de987ab8b
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-06-25 11:46:42 -0300

    fix: corrigindo erro na geração dos relatórios

--- cypress.config.js
+++ cypress.config.js
@@ -1,29 +1,27 @@
 const { defineConfig } = require("cypress");
-const dayjs = require("dayjs");
-const reportFolder = `cypress/reports/${dayjs().format("DD-MM-YYYY_HH-mm-ss")}`;
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Relatório de Testes",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    overwrite: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
+      allureWriter(on, config);
+
+      return config;
     },
     baseUrl: "https://bugbank.netlify.app",
-    reporter: "cypress-mochawesome-reporter",
-    reporterOptions: {
-      reportDir: reportFolder,
-      charts: true,
-      reportPageTitle: "Relatório de Testes",
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: false,
-      html: true,
-      json: true,
-      overwrite: false,
-    },
     env: {
-      allureReuseAfterSpec: true,
-      allureResultsPath: "allure-results",
+      allure: true,
+      allureResultsPath: "cypress/allure-results",
     },
   },
 });
